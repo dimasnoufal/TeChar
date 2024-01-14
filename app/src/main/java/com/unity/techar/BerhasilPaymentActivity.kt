eb32a59ee73c7a6d026a0779fce1c84328dc2888
Fiqih Ardiansyah <ardiansyahfiqih47@gmail.com>
2024-01-14 22:31:11 +0700

    first commit

---
+++ app/src/main/java/com/unity/techar/BerhasilPaymentActivity.kt
@@ -0,0 +1,11 @@
+package com.unity.techar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BerhasilPaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_berhasil_payment)
+    }
+}
